3202559752059e0729dd14b4657ea93177126ec1
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-02-12 21:05:48 +0200

    feat: handle login form validation

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,13 +1,31 @@
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import { loginFormSchema, LoginFormType } from '../../types/schemas/login-form-schema';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 export default function Login() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm<LoginFormType>({
+    resolver: zodResolver(loginFormSchema),
+  });
+
+  async function onLoginFormSubmit(values: LoginFormType) {
+    console.log(values);
+  }
+
   return (
     <div className="pt-24">
       <div className="mx-auto h-full max-w-[600px] rounded-lg bg-card-bg p-8 px-10 shadow xl:p-0">
         <h1 className="mb-5 text-center text-xl font-bold leading-tight tracking-tight">
           Sign in to your account
         </h1>
-        <form className="flex flex-col gap-4 md:gap-6">
+        <form
+          onSubmit={handleSubmit(onLoginFormSubmit)}
+          className="flex flex-col gap-4 md:gap-6"
+        >
           <div>
             <label
               htmlFor="email"
@@ -17,11 +35,12 @@ export default function Login() {
             </label>
             <input
               type="email"
-              name="email"
               id="email"
               className="block h-11 w-full rounded-lg border-2 border-border-dark bg-card-bg p-2.5 shadow-sm focus:border-2 focus:border-color-base focus:outline-none focus:ring-color-base active:border-color-base"
               placeholder="name@company.com"
+              {...register('email')}
             />
+            {errors.email && <p className="text-red-400">{errors.email.message}</p>}
           </div>
 
           <div>
@@ -41,18 +60,20 @@ export default function Login() {
             </div>
             <input
               type="password"
-              name="password"
               id="password"
               placeholder="Your Pssword"
               className="block h-11 w-full rounded-lg border-2 border-border-dark bg-card-bg p-2.5 shadow-sm focus:border-2 focus:border-color-base focus:outline-none focus:ring-color-base active:border-color-base"
+              {...register('password')}
             />
+            {errors.password && <p className="text-red-400">{errors.password.message}</p>}
           </div>
 
           <button
+            disabled={isSubmitting}
             type="submit"
-            className="h-11 self-end rounded-lg bg-emerald-600 px-4 py-2 text-center text-sm font-medium text-white hover:bg-emerald-600 focus:outline-none focus:ring-4 focus:ring-emerald-600"
+            className="h-11 w-full self-end rounded-lg bg-emerald-600 px-4 py-2 text-center text-sm font-medium text-white hover:bg-emerald-600 focus:outline-none focus:ring-4 focus:ring-emerald-600"
           >
-            Sign in
+            {isSubmitting ? 'getting you in' : 'Sign in'}
           </button>
 
           <p className="text-sm text-text-dark">
--- src/types/schemas/login-form-schema.ts
+++ src/types/schemas/login-form-schema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const loginFormSchema = z.object({
-  email: z.string().email().min(1, 'Email is required').trim().toLowerCase(),
+  email: z.string().min(1, 'Email is required').email().trim().toLowerCase(),
 
   password: z.string().min(1, 'Password is required'),
 });
